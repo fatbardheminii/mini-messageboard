47636481b7f9f7e0d1ac7bb79226860fc39f5542
fatbardheminii <fatbardh.emini@hotmail.com>
2025-07-06 17:37:47 +0200

    feat: Add core app functionality, setup Express server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const assetsPath = path.join(__dirname, "public");
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express(json));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
